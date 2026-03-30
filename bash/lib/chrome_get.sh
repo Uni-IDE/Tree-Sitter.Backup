b7f6101c4f40b64b0faaa222649b574df3cfc83b
Your Name <you@example.com>
2026-03-30 04:44:50 +0100

    try

--- bash/lib/chrome_get.sh
+++ bash/lib/chrome_get.sh
@@ -2,13 +2,15 @@ detect_chromeos(){
 	bac_name=$chromeshuttle/bac/$this
 
 	if [ -f $bac_name* ];then
-		archive=`basename -- $bac_name*`
-		last_part=`basename -- $bac_name*`
-		extension="${file#*.}"
+		file=`basename -- $bac_name*`
+		last_part="${file#*.}"
+		extension="${last_part#*.}"
 		compress_type=$extension
 		backup_exist=true
 	else
 		echo "backup not found"
 		backup_exist=true
 	fi
+
+	echo $compress_type
 }
